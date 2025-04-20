d1deefc422749417fbe613b7b0684b508fe24e45
bruderj15 <julian.bruder@outlook.com>
2025-04-20 14:02:38 +0200

    main: add materialization

--- src/item_model.rs
+++ src/item_model.rs
@@ -124,8 +124,8 @@ impl ItemModel {
         self
     }
 
-    pub fn price(&mut self, upper_price: f32) -> &mut Self {
-        self.price = Some(upper_price);
+    pub fn price(&mut self, price: f32) -> &mut Self {
+        self.price = Some(price);
         self
     }
 
@@ -208,10 +208,71 @@ impl Into<ItemData> for ItemModel {
     }
 }
 
+impl TryFrom<&[ItemModel]> for ItemModel {
+    type Error = String;
+
+    /// Convert item-events - sorted by latest (first) - to materialized item.
+    fn try_from(item_events: &[ItemModel]) -> Result<Self, Self::Error> {
+        if item_events.is_empty() {
+            Err("Given 'item_events' were empty.".to_string())
+        } else {
+            let mut item_id = None;
+            let mut source_id = None;
+            let mut created = None;
+            let mut event_id = None;
+            let mut state = None;
+            let mut price = None;
+            let mut category = None;
+            let mut name_en = None;
+            let mut description_en = None;
+            let mut name_de = None;
+            let mut description_de = None;
+            let mut url = None;
+            let mut image_url = None;
+            let mut hash = None;
+
+            for event_ref in item_events {
+                let event = event_ref.to_owned();
+                item_id = item_id.or(Some(event.item_id));
+                source_id = source_id.or(event.source_id);
+                created = created.or(event.created);
+                event_id = event_id.or(event.event_id);
+                state = state.or(event.state);
+                price = price.or(event.price);
+                category = category.or(event.category);
+                name_en = name_en.or(event.name_en);
+                description_en = description_en.or(event.description_en);
+                name_de = name_de.or(event.name_de);
+                description_de = description_de.or(event.description_de);
+                url = url.or(event.url);
+                image_url = image_url.or(event.image_url);
+                hash = hash.or(event.hash);
+            }
+
+            Ok(ItemModel {
+                item_id: item_id.unwrap(),
+                source_id,
+                created,
+                event_id,
+                state,
+                price,
+                category,
+                name_en,
+                description_en,
+                name_de,
+                description_de,
+                url,
+                image_url,
+                hash,
+            })
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use rstest::rstest;
     use super::*;
+    use rstest::rstest;
 
     #[test]
     fn should_serialize_item_id_as_pk_with_prefix_item() {
@@ -219,7 +280,7 @@ mod tests {
         let expected = r#""pk":"item#123456""#;
 
         let actual = serde_json::to_string(&item).unwrap();
-        
+
         assert!(actual.contains(expected));
     }
 
@@ -227,7 +288,7 @@ mod tests {
     fn should_deserialize_item_id_as_pk_with_prefix_item() {
         let item_json = r#"{"pk":"item#123456"}"#;
         let expected = ItemModel::new("123456".to_string());
-        
+
         let actual = serde_json::from_str::<ItemModel>(item_json).unwrap();
 
         assert_eq!(actual, expected);
@@ -235,7 +296,9 @@ mod tests {
 
     #[test]
     fn should_serialize_created_as_sk_with_prefix_item() {
-        let item = ItemModel::new("123456".to_string()).created("abcdef".to_string()).to_owned();
+        let item = ItemModel::new("123456".to_string())
+            .created("abcdef".to_string())
+            .to_owned();
         let expected = r#""sk":"item#abcdef""#;
 
         let actual = serde_json::to_string(&item).unwrap();
@@ -246,7 +309,9 @@ mod tests {
     #[test]
     fn should_deserialize_created_as_sk_with_prefix_item() {
         let item_json = r#"{"pk":"item#123456", "sk":"item#abcdef"}"#;
-        let expected = ItemModel::new("123456".to_string()).created("abcdef".to_string()).to_owned();
+        let expected = ItemModel::new("123456".to_string())
+            .created("abcdef".to_string())
+            .to_owned();
 
         let actual = serde_json::from_str::<ItemModel>(item_json).unwrap();
 
@@ -255,7 +320,9 @@ mod tests {
 
     #[test]
     fn should_serialize_source_id_as_party_id_with_prefix_source() {
-        let item = ItemModel::new("123456".to_string()).source_id("abcdef".to_string()).to_owned();
+        let item = ItemModel::new("123456".to_string())
+            .source_id("abcdef".to_string())
+            .to_owned();
         let expected = r#""party_id":"source#abcdef""#;
 
         let actual = serde_json::to_string(&item).unwrap();
@@ -266,7 +333,9 @@ mod tests {
     #[test]
     fn should_deserialize_source_id_as_party_id_with_prefix_source() {
         let item_json = r#"{"pk":"item#123456", "party_id":"source#abcdef"}"#;
-        let expected = ItemModel::new("123456".to_string()).source_id("abcdef".to_string()).to_owned();
+        let expected = ItemModel::new("123456".to_string())
+            .source_id("abcdef".to_string())
+            .to_owned();
 
         let actual = serde_json::from_str::<ItemModel>(item_json).unwrap();
 
@@ -275,7 +344,9 @@ mod tests {
 
     #[test]
     fn should_serialize_event_id_as_event_id_with_prefix_item() {
-        let item = ItemModel::new("123456".to_string()).event_id("abcdef".to_string()).to_owned();
+        let item = ItemModel::new("123456".to_string())
+            .event_id("abcdef".to_string())
+            .to_owned();
         let expected = r#""event_id":"item#abcdef""#;
 
         let actual = serde_json::to_string(&item).unwrap();
@@ -286,7 +357,9 @@ mod tests {
     #[test]
     fn should_deserialize_event_id_as_event_id_with_prefix_item() {
         let item_json = r#"{"pk":"item#123456", "event_id":"item#abcdef"}"#;
-        let expected = ItemModel::new("123456".to_string()).event_id("abcdef".to_string()).to_owned();
+        let expected = ItemModel::new("123456".to_string())
+            .event_id("abcdef".to_string())
+            .to_owned();
 
         let actual = serde_json::from_str::<ItemModel>(item_json).unwrap();
 
@@ -299,7 +372,10 @@ mod tests {
     #[case(ItemState::RESERVED, "RESERVED")]
     #[case(ItemState::SOLD, "SOLD")]
     #[case(ItemState::REMOVED, "REMOVED")]
-    fn should_serialize_state_as_state_with_prefix_item(#[case] state: ItemState, #[case] expected: &str) {
+    fn should_serialize_state_as_state_with_prefix_item(
+        #[case] state: ItemState,
+        #[case] expected: &str,
+    ) {
         let item = ItemModel::new("123456".to_string()).state(state).to_owned();
         let expected = format!(r#""state":"item#{expected}""#);
 
@@ -314,15 +390,20 @@ mod tests {
     #[case("RESERVED", ItemState::RESERVED)]
     #[case("SOLD", ItemState::SOLD)]
     #[case("REMOVED", ItemState::REMOVED)]
-    fn should_deserialize_state_as_state_with_prefix_item(#[case] state: &str, #[case] expected: ItemState) {
+    fn should_deserialize_state_as_state_with_prefix_item(
+        #[case] state: &str,
+        #[case] expected: ItemState,
+    ) {
         let item_json = format!(r#"{{"pk":"item#123456", "state":"item#{state}"}}"#);
-        let expected = ItemModel::new("123456".to_string()).state(expected).to_owned();
+        let expected = ItemModel::new("123456".to_string())
+            .state(expected)
+            .to_owned();
 
         let actual = serde_json::from_str::<ItemModel>(&item_json).unwrap();
 
         assert_eq!(actual, expected);
     }
-    
+
     #[test]
     fn should_convert_model_into_data() {
         let model = ItemModel {
@@ -339,7 +420,9 @@ mod tests {
             description_de: Some("basis".to_string()),
             url: Some("https://foo.bar?item=123456".to_string()),
             image_url: Some("https://foo.bar?item_img=123456".to_string()),
-            hash: Some("1d10a63438fff3ccd4877c2195c0a377a6ee0c8caad97e652b1e69c68b45557b".to_string()),
+            hash: Some(
+                "1d10a63438fff3ccd4877c2195c0a377a6ee0c8caad97e652b1e69c68b45557b".to_string(),
+            ),
         };
         let expected = ItemData {
             item_id: "https://foo.bar#123456".to_string(),
@@ -353,9 +436,68 @@ mod tests {
             url: Some("https://foo.bar?item=123456".to_string()),
             image_url: Some("https://foo.bar?item_img=123456".to_string()),
         };
-        
+
         let actual: ItemData = model.into();
-        
+
         assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn should_materialize_item_events_for_into_model() {
+        let item_events = vec![
+            ItemModel::new("foo#123456".to_string())
+                .created("2010-01-04T12:00:00.001+01:00".to_string())
+                .source_id("https://foo.bar".to_string())
+                .event_id("https://foo.bar#123456#2010-01-04T12:00:00.001+01:00".to_string())
+                .state(ItemState::SOLD)
+                .to_owned(),
+            ItemModel::new("foo#123456".to_string())
+                .created("2010-01-03T12:00:00.001+01:00".to_string())
+                .source_id("https://foo.bar".to_string())
+                .event_id("https://foo.bar#123456#2010-01-03T12:00:00.001+01:00".to_string())
+                .state(ItemState::AVAILABLE)
+                .price(37f32)
+                .to_owned(),
+            ItemModel::new("foo#123456".to_string())
+                .created("2010-01-02T12:00:00.001+01:00".to_string())
+                .source_id("https://foo.bar".to_string())
+                .event_id("https://foo.bar#123456#2010-01-02T12:00:00.001+01:00".to_string())
+                .state(ItemState::AVAILABLE)
+                .price(42f32)
+                .to_owned(),
+            ItemModel::new("foo#123456".to_string())
+                .created("2010-01-01T12:00:00.001+01:00".to_string())
+                .source_id("https://foo.bar".to_string())
+                .event_id("https://foo.bar#123456#2010-01-01T12:00:00.001+01:00".to_string())
+                .state(ItemState::LISTED)
+                .price(42f32)
+                .category("foo".to_string())
+                .name_en("bar".to_string())
+                .description_en("baz".to_string())
+                .name_de("balken".to_string())
+                .description_de("basis".to_string())
+                .url("https://foo.bar?item=123456".to_string())
+                .image_url("https://foo.bar?item_img=123456".to_string())
+                .to_owned(),
+        ];
+        let expected = ItemModel::new("foo#123456".to_string())
+            .created("2010-01-04T12:00:00.001+01:00".to_string())
+            .source_id("https://foo.bar".to_string())
+            .event_id("https://foo.bar#123456#2010-01-04T12:00:00.001+01:00".to_string())
+            .state(ItemState::SOLD)
+            .price(37f32)
+            .category("foo".to_string())
+            .name_en("bar".to_string())
+            .description_en("baz".to_string())
+            .name_de("balken".to_string())
+            .description_de("basis".to_string())
+            .url("https://foo.bar?item=123456".to_string())
+            .image_url("https://foo.bar?item_img=123456".to_string())
+            .to_owned();
+        
+        let actual: Result<ItemModel, String> = ItemModel::try_from(&item_events[..]);
+        
+        assert!(actual.is_ok());
+        assert_eq!(actual.unwrap(), expected)
     }
 }
